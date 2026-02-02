fc6d6e6d8ed45dca06278df2a24c494784bf0c18
Gerald Berongoy <geraldberongoy04@gmail.com>
2026-02-03 02:21:18 +0800

    styling(font): apply urbanist font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Urbanist } from "next/font/google";
+import { Urbanist, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
 const urbanist = Urbanist({
   variable: "--font-urbanist",
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  fallback: [
+    "system-ui",
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "sans-serif",
+  ],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -36,9 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${urbanist.variable} antialiased`}
-      >
+      <body className={`${urbanist.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
